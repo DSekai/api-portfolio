1ba36e22f673a461ebf8936de9f419386f1cc487
Manuel Eduardo Palta Caro <92129971+DSekai@users.noreply.github.com>
2024-07-28 18:01:57 -0400

    Update cors.ts

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -1,6 +1,6 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:1234', 'https://sekaidev.com', 'https://www.sekaidev.com', 'http://www.sekaidev.com', 'http://sekaidev.com', 'http://www.port.sekaidev.com', 'http://port.sekaidev.com','https://sekaidev.netlify.app']
+const ACCEPTED_ORIGINS = ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:1234', 'https://sekaidev.com', 'https://www.sekaidev.com', 'http://www.sekaidev.com', 'http://sekaidev.com', 'http://www.port.sekaidev.com', 'http://port.sekaidev.com','http://sekaidev.netlify.app','https://sekaidev.netlify.app']
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
   origin: (origin, callback) => {
